714aa004c62f0d482d2e26788a416c4ffd5f7aee
John Kelleher <johnpkelleher@gmail.com>
2022-04-23 18:52:02 -0400

    wip

---
+++ packages/hardhat/scripts/deployLensProfile.js
@@ -0,0 +1,29 @@
+import { task } from 'hardhat/config';
+import { LensHub__factory } from '../typechain-types';
+import { CreateProfileDataStruct } from '../typechain-types/LensHub';
+import { waitForTx, initEnv, getAddrs, ZERO_ADDRESS } from './helpers/utils';
+
+
+const { ethers } = require("hardhat");
+
+task('create-profile', 'creates a profile').setAction(async ({}, hre) => {
+
+  const inputStruct: CreateProfileDataStruct = {
+    to: user.address,
+    handle: 'zer0dot',
+    imageURI:
+      'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
+    followModule: ZERO_ADDRESS,
+    followModuleData: [],
+    followNFTURI:
+      'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan',
+  };
+
+  await waitForTx(lensHub.connect(user).createProfile(inputStruct));
+
+  console.log(`Total supply (should be 1): ${await lensHub.totalSupply()}`);
+  console.log(
+    `Profile owner: ${await lensHub.ownerOf(1)}, user address (should be the same): ${user.address}`
+  );
+  console.log(`Profile ID by handle: ${await lensHub.getProfileIdByHandle('zer0dot')}`);
+});
